56851319a2de47b73f628673700110688284a12b
Steven Bleau <bleausd@gmail.com>
2022-09-12 16:20:44 -0500

    goat pictures added

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,8 @@
 const galleryItems = [
     { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 2, path: 'images/goat1.jpeg', description: 'Photo of a goat', likes: 0 }
+    { id: 3, path: 'images/goat2.webp', description: 'Photo of a goat', likes: 0 }
+    { id: 4, path: 'images/goat3.webp', description: 'Photo of a goat', likes: 0 }
 ];
 
 module.exports = galleryItems;
